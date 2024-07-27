73cfa94105c1152c92ffedd5f301f79d6ee435e0
Arnaud ISSOIRE <72009426+aIssoire@users.noreply.github.com>
2024-07-27 17:42:54 +0700

    🟢add: let user connected when reload the app

--- Solervis/Solervis/Models/UserSettings.swift
+++ Solervis/Solervis/Models/UserSettings.swift
@@ -2,5 +2,21 @@ import SwiftUI
 import Combine
 
 class UserSettings: ObservableObject {
-    @Published var isUserLoggedIn: Bool = false
+    @Published var isUserLoggedIn: Bool = UserDefaults.standard.bool(forKey: "isUserLoggedIn") {
+        didSet {
+            UserDefaults.standard.set(self.isUserLoggedIn, forKey: "isUserLoggedIn")
+        }
+    }
+    
+    @Published var token: String? {
+        didSet {
+            UserDefaults.standard.set(self.token, forKey: "token")
+        }
+    }
+    
+    @Published var userId: String? {
+        didSet {
+            UserDefaults.standard.set(self.userId, forKey: "userId")
+        }
+    }
 }
--- Solervis/Solervis/Views/LoginView.swift
+++ Solervis/Solervis/Views/LoginView.swift
@@ -7,66 +7,67 @@ struct LoginView: View {
     @EnvironmentObject var userSettings: UserSettings
 
     var body: some View {
-        NavigationView {
-            VStack {
-                Spacer()
+        VStack {
+            Spacer()
+            
+            Image("solervis_logo") // Assurez-vous que l'image du logo est ajoutée à vos assets
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 100)
+            
+            Spacer()
+            
+            VStack(spacing: 20) {
+                CustomTextField(placeholder: "E-Mail", imageName: "envelope", text: $email)
+                    .padding()
+                    .background(Color.yellow.opacity(0.3))
+                    .cornerRadius(8)
                 
-                Image("solervis_logo") // Assurez-vous que l'image du logo est ajoutée à vos assets
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 100)
-                
-                Spacer()
-                
-                VStack(spacing: 20) {
-                    CustomTextField(placeholder: "E-Mail", imageName: "envelope", text: $email)
-                        .padding()
-                        .background(Color.yellow.opacity(0.3))
-                        .cornerRadius(8)
-                    
-                    CustomTextField(placeholder: "Mot de passe", imageName: "lock", text: $password, isSecure: true)
-                        .padding()
-                        .background(Color.yellow.opacity(0.3))
-                        .cornerRadius(8)
-                }
-                .padding(.horizontal, 32)
-                
-                if let errorMessage = errorMessage {
-                    Text(errorMessage)
-                        .foregroundColor(.red)
-                        .padding()
-                }
-                
-                Button(action: {
-                    login()
-                }) {
-                    Text("Connexion")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.orange)
-                        .cornerRadius(8)
-                }
-                .padding(.horizontal, 32)
-                .padding(.top, 20)
-                
-                Spacer()
-                
-                Button(action: {
-                    // Action pour s'inscrire
-                }) {
-                    Text("Pas encore de compte ?")
-                        .foregroundColor(.orange)
-                        .padding()
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.orange, lineWidth: 2)
-                        )
-                }
-                .padding(.horizontal, 32)
-                .padding(.bottom, 30)
+                CustomTextField(placeholder: "Mot de passe", imageName: "lock", text: $password, isSecure: true)
+                    .padding()
+                    .background(Color.yellow.opacity(0.3))
+                    .cornerRadius(8)
             }
+            .padding(.horizontal, 32)
+            
+            if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            Button(action: {
+                login()
+            }) {
+                Text("Connexion")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange)
+                    .cornerRadius(8)
+            }
+            .padding(.horizontal, 32)
+            .padding(.top, 20)
+            
+            Spacer()
+            
+            Button(action: {
+                // Action pour s'inscrire
+            }) {
+                Text("Pas encore de compte ?")
+                    .foregroundColor(.orange)
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.orange, lineWidth: 2)
+                    )
+            }
+            .padding(.horizontal, 32)
+            .padding(.bottom, 30)
+        }
+        .onAppear {
+            checkIfLoggedIn()
         }
     }
     
@@ -94,13 +95,54 @@ struct LoginView: View {
                    let token = json["token"] as? String,
                    let userId = json["id"] as? String {
                     DispatchQueue.main.async {
-                        UserDefaults.standard.set(userId, forKey: "userId")
-                        UserDefaults.standard.set(token, forKey: "token")
+                        self.userSettings.userId = userId
+                        self.userSettings.token = token
                         self.userSettings.isUserLoggedIn = true
                     }
                 } else {
                     DispatchQueue.main.async {
                         self.errorMessage = "Invalid login credentials"
+                    }
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.errorMessage = "Failed to decode response"
+                }
+            }
+        }.resume()
+    }
+
+    func checkIfLoggedIn() {
+        if let token = UserDefaults.standard.string(forKey: "token") {
+            refreshAuthToken(token: token)
+        }
+    }
+    
+    func refreshAuthToken(token: String) {
+        guard let url = URL(string: "https://solervis.fr/api/user/auth/refresh") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                   let newToken = json["token"] as? String {
+                    DispatchQueue.main.async {
+                        self.userSettings.token = newToken
+                        self.userSettings.isUserLoggedIn = true
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.errorMessage = "Failed to refresh token"
                     }
                 }
             } catch {
